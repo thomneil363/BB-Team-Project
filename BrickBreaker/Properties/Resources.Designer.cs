4729c1c8364e894ca28669d7f44c7cd2f60e820f
Christopher Phillis <ChrisPhillis@outlook.com>
2020-10-21 09:56:44 -0400

    Updated highscore screen

--- BrickBreaker/Properties/Resources.Designer.cs
+++ BrickBreaker/Properties/Resources.Designer.cs
@@ -143,6 +143,16 @@ namespace BrickBreaker.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap pinkBrick {
+            get {
+                object obj = ResourceManager.GetObject("pinkBrick", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap playButtonSprite {
             get {
                 object obj = ResourceManager.GetObject("playButtonSprite", resourceCulture);
