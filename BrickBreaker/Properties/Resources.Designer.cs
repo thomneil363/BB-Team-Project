ad902209f9a49122022465153b44a64a2de710c3
Christopher Phillis <ChrisPhillis@outlook.com>
2020-10-21 10:38:22 -0400

    update

--- BrickBreaker/Properties/Resources.Designer.cs
+++ BrickBreaker/Properties/Resources.Designer.cs
@@ -123,6 +123,16 @@ namespace BrickBreaker.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap highscoreTitleSprite {
+            get {
+                object obj = ResourceManager.GetObject("highscoreTitleSprite", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap orangeBrick {
             get {
                 object obj = ResourceManager.GetObject("orangeBrick", resourceCulture);
--- BrickBreaker/Screens/HighscoresScreen.cs
+++ BrickBreaker/Screens/HighscoresScreen.cs
@@ -16,5 +16,25 @@ namespace BrickBreaker
         {
             InitializeComponent();
         }
+
+        #region if space is pressed go back to the menu
+        private void HighscoresScreen_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            //player button presses
+            switch (e.KeyCode)
+            {
+                case Keys.Space:
+                    // Goes to the menu screen
+                    MenuScreen ms = new MenuScreen();
+                    Form form = this.FindForm();
+
+                    form.Controls.Add(ms);
+                    form.Controls.Remove(this);
+
+                    ms.Location = new Point((form.Width - ms.Width) / 2, (form.Height - ms.Height) / 2);
+                    break;
+            }
+        }
+        #endregion if space is pressed go back to the menu
     }
 }
