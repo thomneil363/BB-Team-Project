fabca2507911fa791b6e576b37abfe04d6049c0f
Christopher Phillis <ChrisPhillis@outlook.com>
2020-10-22 08:57:04 -0400

    update

--- BrickBreaker/Screens/GameOverScreen.Designer.cs
+++ BrickBreaker/Screens/GameOverScreen.Designer.cs
@@ -28,93 +28,112 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.gameoverTitleLabel = new System.Windows.Forms.Label();
-            this.brick1 = new System.Windows.Forms.Label();
-            this.brick2 = new System.Windows.Forms.Label();
-            this.brick3 = new System.Windows.Forms.Label();
-            this.brick4 = new System.Windows.Forms.Label();
             this.exitLabel = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
             this.SuspendLayout();
-            // 
-            // gameoverTitleLabel
-            // 
-            this.gameoverTitleLabel.Image = global::BrickBreaker.Properties.Resources.gameoverTitleSprite;
-            this.gameoverTitleLabel.Location = new System.Drawing.Point(250, 75);
-            this.gameoverTitleLabel.Name = "gameoverTitleLabel";
-            this.gameoverTitleLabel.Size = new System.Drawing.Size(350, 330);
-            this.gameoverTitleLabel.TabIndex = 1;
-            // 
-            // brick1
-            // 
-            this.brick1.Image = global::BrickBreaker.Properties.Resources.pinkBrick;
-            this.brick1.Location = new System.Drawing.Point(62, 301);
-            this.brick1.Name = "brick1";
-            this.brick1.Size = new System.Drawing.Size(61, 33);
-            this.brick1.TabIndex = 2;
-            // 
-            // brick2
-            // 
-            this.brick2.Image = global::BrickBreaker.Properties.Resources.pinkBrick;
-            this.brick2.Location = new System.Drawing.Point(683, 233);
-            this.brick2.Name = "brick2";
-            this.brick2.Size = new System.Drawing.Size(61, 33);
-            this.brick2.TabIndex = 3;
-            // 
-            // brick3
-            // 
-            this.brick3.Image = global::BrickBreaker.Properties.Resources.pinkBrick;
-            this.brick3.Location = new System.Drawing.Point(167, 111);
-            this.brick3.Name = "brick3";
-            this.brick3.Size = new System.Drawing.Size(61, 33);
-            this.brick3.TabIndex = 4;
-            // 
-            // brick4
-            // 
-            this.brick4.Image = global::BrickBreaker.Properties.Resources.pinkBrick;
-            this.brick4.Location = new System.Drawing.Point(570, 26);
-            this.brick4.Name = "brick4";
-            this.brick4.Size = new System.Drawing.Size(61, 33);
-            this.brick4.TabIndex = 5;
             // 
             // exitLabel
             // 
             this.exitLabel.AutoSize = true;
             this.exitLabel.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.exitLabel.ForeColor = System.Drawing.Color.Fuchsia;
-            this.exitLabel.Location = new System.Drawing.Point(298, 454);
+            this.exitLabel.Location = new System.Drawing.Point(282, 453);
             this.exitLabel.Name = "exitLabel";
             this.exitLabel.Size = new System.Drawing.Size(259, 50);
             this.exitLabel.TabIndex = 7;
             this.exitLabel.Text = "Press SPACE to go back\r\nto the title screen. . .";
             this.exitLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::BrickBreaker.Properties.Resources.gameoverTitleSprite;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(250, 80);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(317, 305);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = global::BrickBreaker.Properties.Resources.pinkBrick;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.Location = new System.Drawing.Point(161, 110);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(63, 33);
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = global::BrickBreaker.Properties.Resources.pinkBrick;
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox3.Location = new System.Drawing.Point(64, 309);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(56, 26);
+            this.pictureBox3.TabIndex = 10;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackgroundImage = global::BrickBreaker.Properties.Resources.pinkBrick;
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox4.Location = new System.Drawing.Point(579, 29);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(63, 33);
+            this.pictureBox4.TabIndex = 11;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackgroundImage = global::BrickBreaker.Properties.Resources.pinkBrick;
+            this.pictureBox5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox5.Location = new System.Drawing.Point(673, 233);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(56, 26);
+            this.pictureBox5.TabIndex = 12;
+            this.pictureBox5.TabStop = false;
+            // 
             // GameOverScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.Color.Black;
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.exitLabel);
-            this.Controls.Add(this.brick4);
-            this.Controls.Add(this.brick3);
-            this.Controls.Add(this.brick2);
-            this.Controls.Add(this.brick1);
-            this.Controls.Add(this.gameoverTitleLabel);
             this.DoubleBuffered = true;
             this.Name = "GameOverScreen";
             this.Size = new System.Drawing.Size(850, 550);
             this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.GameOverScreen_PreviewKeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label gameoverTitleLabel;
-        private System.Windows.Forms.Label brick1;
-        private System.Windows.Forms.Label brick2;
-        private System.Windows.Forms.Label brick3;
-        private System.Windows.Forms.Label brick4;
         private System.Windows.Forms.Label exitLabel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
     }
 }
